83ecc142f58009b485091e5d4f16a104d3389a2b
roji-tech <rojitech9@gmail.com>
2022-07-29 01:33:02 +0100

    added

--- storefront/settings/dev.py
+++ storefront/settings/dev.py
@@ -4,13 +4,11 @@ DEBUG = True
 
 SECRET_KEY = 'django-insecure-hs6j037urx6iav+7#10%-vu4l4f5@@-1_zo)oft4g7$vf2$jmp'
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'storefront3',
-        'HOST': 'mysql',
-        'USER': 'root',
-        'PASSWORD': 'MyPassword'
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
